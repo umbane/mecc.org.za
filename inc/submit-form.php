da3d93e454ceee6348b092e9edda2344ef61c2b9
ubuntupunk <ubuntupunk@gmail.com>
2023-07-17 18:25:26 +0200

    serverside commit

--- inc/submit-form.php
+++ inc/submit-form.php
@@ -1,7 +1,7 @@
 <?php
 
 // Sanitize form inputs
-$firstname = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $surname = filter_input(INPUT_POST, 'surname', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 $issue = filter_input(INPUT_POST, 'issue', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -17,18 +17,18 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 $mysqli = new mysqli('localhost', 'sexthera_contact_form_user', 'R@ZqV4iD^!T-', 'sexthera_contact_form_db');
 
 // Insert the user input data into the database
-$query = "INSERT INTO form_submissions (name, surname, email, issue, message) VALUES (?, ?, ?, ?, ?)";
+$query = "INSERT INTO contact_form (name, surname, email, issue, message) VALUES (?, ?, ?, ?, ?)";
 $stmt = $mysqli->prepare($query);
 $stmt->bind_param('sssss', $name, $surname, $email, $issue, $message);
 $stmt->execute();
 
 // Send an email to the webmaster
-$to = "webmaster@example.com";
+$to = "hello@mecc.co.za";
 $subject = "New form submission";
 $body = "Issue: $issue\n\nMessage:\n$message";
 mail($to, $subject, $body);
 
-// Redirect the user to athank you page
+// Redirect the user to a thank you page
 header("Location: thank-you.html");
 exit();
 ?>
